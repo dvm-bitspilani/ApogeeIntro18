b7530cc81383091a0893e6b0584bcd35a9238759
pallav <pallavsoni8800@yahoo.com>
2017-12-19 16:04:38 +0530

    paper portal and paper section linked

--- Js/navbar.js
+++ Js/navbar.js
@@ -22,11 +22,11 @@ function closeinfo() {
 
 
  var div = document.createElement('div');
-    div.innerHTML = '<img class="img" src="Image/paper.svg" ><p class="infop">Paper presentation forms the core event of APOGEE that showcases a wide range of technical and review papers on a spectrum of topics ranging from industrial applications to the very latest scientific innovations.<br><br><a class="link2" href="#">Click here to know more</a></p>';
+    div.innerHTML = '<img class="img" src="Image/paper.svg" ><p class="infop">Paper presentation forms the core event of APOGEE that showcases a wide range of technical and review papers on a spectrum of topics ranging from industrial applications to the very latest scientific innovations.<br><br><a class="link2" href="https://bits-apogee.org/2018/portal" target="_blank">Click here to know more (paper and projects portal)</a></p>';
      document.getElementById('pap').appendChild(div);
      div.id = "paperinrcontainer";
 
  var div = document.createElement('div');
-    div.innerHTML = '<p class="infop">APOGEE exhibits over 550 projects in various fields of science and engineering. Based on practical application, they showcase unparalleled excellence. Some of the categories of Projects include Electronics, Environment, Software Development and Mathematical Modelling.. <br> <b>Abstract submission for non BITSian participants is open from 14th December 2016 to 7th January 2017.</b><br><br><span class="right" style="text-align:right"><a class="link2" href="#">Click here to know more</a></span></p><img class="img" src="Image/projects.svg" >';
+    div.innerHTML = '<p class="infop">APOGEE exhibits over 550 projects in various fields of science and engineering. Based on practical application, they showcase unparalleled excellence. Some of the categories of Projects include Electronics, Environment, Software Development and Mathematical Modelling.. <br> <b>Abstract submission for non BITSian participants is open from 14th December 2016 to 7th January 2017.</b><br><br><span class="right" style="text-align:right"><a class="link2" href="https://bits-apogee.org/2018/portal" target="_blank">Click here to know more (paper and projects portal)</a></span></p><img class="img" src="Image/projects.svg" >';
      document.getElementById('pre').appendChild(div);
      div.id = "projectinrcontainer";
